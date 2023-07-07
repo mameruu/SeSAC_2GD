d182d40b449e6c982aa1c0a21d14d04896bc9e1a
mameruu <gkswjddbs95@naver.com>
2023-07-07 15:42:10 +0900

    day12

--- js_dom/event.js
+++ js_dom/event.js
@@ -48,3 +48,73 @@ window.addEventListener("scroll", () => {
     //스크롤 이벤트는 윈도우에 걸어야함
   }
 });
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+btn.addEventListener("click", function (event) {
+  console.log(event);
+});
+
+input.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  console.log(event.code);
+  // 방향키 위 아래 왼쪽 오른쪽을 누르면
+  // 각각 누를때마다 업 다운 레프트 라이트
+  if (event.key === "ArrowUp") {
+    console.log("up");
+  } else if (event.key === "AroowDown") {
+    console.log("down");
+  } else if (event.key === "ArrowLeft") {
+    console.log("left");
+  } else if (event.key === "ArrowRight") {
+    //else 하면 안된느 이유 다른 키들도 다 right값을 받을테니까.
+    console.log("right");
+  } else {
+    console.log("others");
+  }
+});
+
+//변경 이벤트
+const chginput = document.getElementById("change-input");
+chginput.addEventListener("input", () => {
+  console.log("변경중!!");
+  let div = document.querySelector(".box");
+  div.innerHTML = chginput.value;
+});
+// input :현재 변경사항이 존재하는 순간을 계쏙 인식
+// 실시간으로 바로 반영할 수 있음.
+// shift,ctrl.등 입력값이 변하지 않는다면 이벤트 발생 x
+
+chginput.addEventListener("change", () => {
+  // 'change' :input에 변경이 일어나고
+  // 다른 요소를 클릭해서 focus on 시키면 이벤ㄴ트 발생함
+  console.log("변경되었어요!");
+});
+
+// 폼이 제출되는 이벤트
+// 'submit' form ㅔ 적용하는 이벤트
+const todoForm = document.querySelector("#todo-form");
+const todos = document.querySelector(".todos");
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //폼이 실제로 제출되지 않도록 이벤트를 막는 것.
+  // 새로고침 방지 (새로고침 하묜 )
+  const todoInput = document.querySelector('[name="todo"]');
+  console.log(todoInput.value);
+
+  const todo = todoInput.value;
+
+  const li = document.createElement("li");
+  //<li></li>
+  // li.textContent = todo;
+  li.append(todo);
+  // <li>안녕하세요</li>
+  todos.append(li);
+
+  todoInput.value = "";
+  //만약에 제출할 때 빈칸이나 공백 문자가 들어왔을 때는 추가하고 싶지 않다면?
+
+  // const i = todoInput.value;
+  // if (i === "") { }
+});
